5b957601ed6c1a4de9e76a6e7c53c9fe22dec450
Kyle Kukkola <kkukkola33@gmail.com>
2022-12-07 19:25:31 -0800

    integrated with sql database removed hardcoded data values

---
+++ db.ts
@@ -0,0 +1,38 @@
+import mysql from 'mysql'
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'testuser',
+    password: 'Password33',
+    database: 'activity_db'
+})
+
+export function GetUsers() {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM users;`, 
+        function(err, results, fields) {
+            return err ? reject(err) : resolve(results);
+        });
+    })
+}
+
+export function AddUser(userId: Number) {
+    return new Promise((resolve, reject) => {
+        connection.query("INSERT INTO users VALUES(?,?)", [userId, "NAME"], 
+        function(err, results, fields) {
+            return err ? reject(err) : resolve(results);
+        });
+    })
+}
+
+export function DeleteUser(userId: Number) {
+    return new Promise((resolve, reject) => {
+        connection.query("DELETE FROM users WHERE userId = ?", userId,
+        function(err, results, fields) {
+            return err ? reject(err) : resolve(results);
+        });
+    })
+}
+
+export function addUser(id: Number) {
+    console.log('addUser' + id);
+}
--- index.ts
+++ index.ts
@@ -1,14 +1,63 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import * as db from './db'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
+
 app.get('/', function(req: Request, res: Response) {
     res.send("Express + TypeScript Server");
 })
+
+app.get('/api/users', async function(req: Request, res: Response) {
+    try {
+        db.GetUsers()
+            .then(results => { res.send(results); })
+            .catch(err => { res.json({ message: err.message }); })
+    } catch (err) {
+        res.status(500).json({ message: (err as Error).message });
+    }
+})
+
+app.put('/api/users/:id', function(req: Request, res: Response) {
+    // Look up the user 
+    // If not existing, 404
+    // If bad req, 400
+    // Return the updated course
+})
+
+app.post('/api/users', function(req: Request, res: Response) {
+    try {
+        // TODO: make sure that the user actually exists on roblox side (AND GET THE NAME)
+        const NAME = "NAME";
+        db.AddUser(parseInt(req.body.userId))
+            .then(results=>res.send({ ...req.body, name: NAME }))
+            .catch(err => res.json({ message: err.message }))
+    } catch(err) {
+        res.json({ message: (err as Error).message });
+    }
+})
+
+app.delete('/api/users/:id', function(req: Request, res: Response) {
+    try {
+        db.DeleteUser(parseInt(req.params.id))
+            .then(results => res.send(results))
+            .catch(err => res.send({ message: err.message }))
+    } catch(err) {
+        res.send(500).json({ message: (err as Error).message })
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`🔔[server]: Server is running at https://localhost:${port}`);
